fb2387f4b0b9c20b94aad1d0d210aafebc8a2f1b
tyakamyz <tyakamyz@naver.com>
2022-04-06 14:23:26 +0900

    feat: Player 팩토리 생성

--- src/main/java/nextstep/blackjack/Player.java
+++ src/main/java/nextstep/blackjack/Player.java
@@ -1,6 +1,11 @@
 package nextstep.blackjack;
 
 public abstract class Player {
+    enum Type{
+        DEALER,
+        GAMER
+    }
+
     public Cards cards;
 
     public abstract void startCardsSetting(Cards cardDeck);
--- src/main/java/nextstep/blackjack/PlayerFactory.java
+++ src/main/java/nextstep/blackjack/PlayerFactory.java
@@ -1,4 +1,17 @@
 package nextstep.blackjack;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PlayerFactory {
+    private static final Map<Player.Type, Player> creators = new HashMap<>();
+
+    static {
+        creators.put(Player.Type.DEALER, new Dealer());
+        creators.put(Player.Type.GAMER, new Gamer());
+    }
+
+    Player create(Player.Type type){
+        return creators.get(type);
+    }
 }
