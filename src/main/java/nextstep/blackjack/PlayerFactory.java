4ca5761d019a6f0d6e711bbd8baaf3dc21e2685b
tyakamyz <tyakamyz@naver.com>
2022-04-06 14:30:33 +0900

    feat: 예외 적용

--- src/main/java/nextstep/blackjack/PlayerFactory.java
+++ src/main/java/nextstep/blackjack/PlayerFactory.java
@@ -12,6 +12,12 @@ public class PlayerFactory {
     }
 
     Player create(Player.Type type){
+        Player player = creators.get(type);
+
+        if (player == null) {
+            throw new IllegalArgumentException("유효하지 않은 타입입니다.");
+        }
+
         return creators.get(type);
     }
 }
